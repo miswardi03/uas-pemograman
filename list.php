4985721246ff5cdcae00dcff60f45d355a90d138
miswardi03 <123065084+miswardi03@users.noreply.github.com>
2023-01-19 16:25:01 +0700

    Add files via upload

--- list.php
+++ list.php
@@ -1,6 +1,9 @@
 <?php 
- $connection = new mysqli("localhost","root","","latihan");
- $data = mysqli_query($connection, "select * from flutter_upload_images");
- $data = mysqli_fetch_all($data, MYSQLI_ASSOC);
- echo json_encode($data);
+
+    $connection = new mysqli("localhost","root","","latihan_flutter");
+    $data       = mysqli_query($connection, "select * from note_app");
+    $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
+
+    echo json_encode($data);
+	
 ?>
